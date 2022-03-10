615499f21303e92491744338551ddf618f468e76
Yisake Beyene <tadeleyisake@gmail.com>
2022-03-09 23:23:05 -0500

    Finished the required part of Chapter 3

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -16,30 +16,24 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true{
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+        
+    }
+    
     @IBAction func onLoginButton(_ sender: Any) {
-        
+
         let myUrl = "https://api.twitter.com/oauth/request_token"
-        print("Button clicked")
-        
+
         TwitterAPICaller.client?.login(url: myUrl, success: {
-            print("It worked!")
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("Could not log in!")
         })
-        
-        print("After the button clicked")
-        
-    }
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "Q5675QmwJUgdfAR96qW2pvn90", consumerSecret: "7EYAlOyJaqaW479gX1wu0ga7omaHAmJkfyxPfXgog8g1IveSHJ")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
