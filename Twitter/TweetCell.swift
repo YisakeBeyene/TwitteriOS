823300f614c81aeff286a909a0acb2c1ce32f4f3
Yisake Beyene <tadeleyisake@gmail.com>
2022-03-18 15:57:09 -0400

    Added compose tweet, favorite tweet and icon for retweet

--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -14,6 +14,46 @@ class TweetCell: UITableViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var favButton: UIButton!
+    
+    var favorited:Bool = false
+    var tweetID:Int = -1
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        
+        let toBeFavorited = !favorited
+        if(toBeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetID, success: {
+                self.setFavorited(true)
+            }, failure: { Error in
+                print("Favorite did not succeed: \(Error)")
+            })
+        }else{
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetID, success: {
+                self.setFavorited(false)
+            }, failure: { Error in
+                print("Unfavorite did not succeed: \(Error)")
+            })
+        }
+        
+    }
+    
+    @IBAction func retweet(_ sender: Any) {
+        
+        
+        
+    }
+    
+    func setFavorited(_ isFavorited:Bool){
+        favorited = isFavorited
+        if(favorited){
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        }else{
+            favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
